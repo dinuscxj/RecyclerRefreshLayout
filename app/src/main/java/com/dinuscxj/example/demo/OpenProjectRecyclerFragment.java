f9cb1fb2d3b87107137cd1f4741943085bf6ab5f
dinuscxj <13836160245@163.com>
2016-08-28 22:25:51 +0800

    fix demo bug

--- app/src/main/java/com/dinuscxj/example/demo/OpenProjectRecyclerFragment.java
+++ app/src/main/java/com/dinuscxj/example/demo/OpenProjectRecyclerFragment.java
@@ -7,6 +7,7 @@ import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -93,7 +94,7 @@ public class OpenProjectRecyclerFragment extends RecyclerFragment<OpenProjectMod
 
     @Override
     protected RecyclerView.LayoutManager onCreateLayoutManager() {
-        return new GridLayoutManager(getActivity(), 2);
+        return new LinearLayoutManager(getActivity());
     }
 
     @Override
