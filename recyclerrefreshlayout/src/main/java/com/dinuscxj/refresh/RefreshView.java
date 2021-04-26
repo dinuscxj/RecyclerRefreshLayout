8f90395135459f15f00f46b9e4900c6f139c56b3
mhelder <matthias.helder@justpinch.com>
2021-04-26 17:01:15 +0200

    Remove unused support v4 import

--- recyclerrefreshlayout/src/main/java/com/dinuscxj/refresh/RefreshView.java
+++ recyclerrefreshlayout/src/main/java/com/dinuscxj/refresh/RefreshView.java
@@ -6,7 +6,6 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
-import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
